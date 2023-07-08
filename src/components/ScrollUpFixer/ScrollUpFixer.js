5de7386646b666d5c7baa817940c4f6ccea4035e
pablokopp <84154261+pablokopp@users.noreply.github.com>
2023-07-07 22:24:59 -0300

    smooth scroll

--- src/components/ScrollUpFixer/ScrollUpFixer.js
+++ src/components/ScrollUpFixer/ScrollUpFixer.js
@@ -3,7 +3,7 @@ import { useLayoutEffect } from "react";
 const ScrollUpFixer = ({ children, actLuna }) => {
   useLayoutEffect(() => {
     if (actLuna !== 0) {
-      document.documentElement.scrollTo(0, 3450);
+      document.documentElement.scrollTo({ top: 3450, behavior: "smooth" });
     }
   }, [actLuna]);
 
